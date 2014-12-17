bda1fd16e2f72e06ad6477f24cbcecefd35f935d
rimager <rima.gerhard@gmail.com>
2014-12-17 12:57:18 -0500

    Initial Commit
    Committing index.html, Gruntfile, css and js

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        tag: {
+            banner: '/*!\n' +
+                ' * <%= pkg.name %>\n' +
+                ' * <%= pkg.title %>\n' +
+                ' * <%= pkg.url %>\n' +
+                ' * @author <%= pkg.author %>\n' +
+                ' * @version <%= pkg.version %>\n' +
+                ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+                ' */\n'
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            vendors_footer: {
+                files:{
+                    "dist/js/footer.min.js" : [ "js/lib/masonry/dist/masonry.pkgd.min.js"]
+                }
+            },
+            vendors_head: {
+                files:{
+                    "dist/js/modernizr.min.js" : [ "js/lib/modernizr/modernizr.js"]
+                }
+            }
+            /*
+            vendors_normalize: {
+                files:{
+                    "dist/css/normalize.min.css" : "js/lib/normalize.css/normalize.css"
+                }
+            }
+            */
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    //{expand: true, src: ['js/lib/modernizr/modernizr.js'], dest: 'dest/', filter: 'isFile'},
+
+                ]
+            }
+        },
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    banner: '<%= tag.banner %>',
+                    compass: true
+                },
+                files: {
+                    'dist/css/style.css': 'scss/style.scss'
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed',
+                    compass: true
+                },
+                files: {
+                    'dist/css/style.css': 'scss/style.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: 'scss/{,*/}*.{scss,sass}',
+                tasks: ['sass:dev']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'sass:dev', 'watch']);
+
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+js
+==
+
+A collection of Javascript projects using various libraries:
+
+* modernizr
+* masonry
+
+Also using bower to retrieve Libraries and Grunt to minify and move to the dist/js folder.
